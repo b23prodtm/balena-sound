aa34f8ecfe2b13f0d65af59cddcf148f09eaae40
Tiana Rakoto Arimanana <5566338+b23prodtm@users.noreply.github.com>
2020-06-01 12:22:05 +0200

    Fixes: Ci Tests Fixes: exec format error Adds: Status badge

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 ### Paste in here latest File Revisions
 REV=https://raw.githubusercontent.com/b23prodtm/vagrant-shell-scripts/b23prodtm-patch/vendor/cni/balena_deploy.sh
-#REV=https://raw.githubusercontent.com/b23prodtm/vagrant-shell-scripts/87e48481c955e213de3d08453dd4dd56d1104bec/vendor/cni/balena_deploy.sh
+#REV=https://raw.githubusercontent.com/b23prodtm/vagrant-shell-scripts/36a8e73ed1c1a8eca501ddbbfdf73a5238a45ef4/vendor/cni/balena_deploy.sh
 sudo curl -SL -o /usr/local/bin/balena_deploy $REV
 sudo chmod 0755 /usr/local/bin/balena_deploy
 source balena_deploy ${BASH_SOURCE[0]} "$@"
